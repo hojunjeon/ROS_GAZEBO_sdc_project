0d1aa076ba3deaeded257c0c4007567c0134ac80
hojun <hoj0902@naver.com>
2025-08-12 12:51:12 +0000

    1. prius_hybrid_sensor : 라이다 등 센서 부착   2. 테스트월드 트랙 및 시나리오 구조물, 추가차량 배치    3. 노드 발행토픽 수정 : namespace부여 및 remapping으로 차량이 개별토픽 구독 가능

--- self_driving_car_pkg/self_driving_car_pkg/computer_vision_node.py
+++ self_driving_car_pkg/self_driving_car_pkg/computer_vision_node.py
@@ -11,8 +11,8 @@ class Video_feed_in(Node):
     def __init__(self):
 
         super().__init__('video_subscriber')
-        self.subscriber = self.create_subscription(Image,'/camera/image_raw',self.process_data,10)
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 40)
+        self.subscriber = self.create_subscription(Image,'/prius_hybrid_sensor/camera/image_raw',self.process_data,10)
+        self.publisher = self.create_publisher(Twist, '/prius_hybrid_sensor/cmd_vel', 40)
         timer_period = 0.5;self.timer = self.create_timer(timer_period, self.send_cmd_vel)
 
         self.velocity = Twist()
