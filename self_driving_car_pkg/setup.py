5448153d19ef57b5960836dc014a79a371ddaa39
hojun <hoj0902@naver.com>
2025-08-21 06:49:42 +0000

    최종 : 1. 신호등/표지판/차간거리유지 기능 정상화 2. 각 기능별 테스트 환경 월드 추가(발표자료용)

--- self_driving_car_pkg/setup.py
+++ self_driving_car_pkg/setup.py
@@ -48,9 +48,9 @@ setup(
         'console_scripts': [
         'spawner_node = self_driving_car_pkg.sdf_spawner:main',
         'computer_vision_node = self_driving_car_pkg.computer_vision_node:main',
-        'video_recording_node = self_driving_car_pkg.video_save:main',
-        'upper_camera_recording = self_driving_car_pkg.upper_camera_video:main',
-        'sdc_V2 = self_driving_car_pkg.sdc_V2:main',
+        #'video_recording_node = self_driving_car_pkg.video_save:main',
+        #'upper_camera_recording = self_driving_car_pkg.upper_camera_video:main',
+        #'sdc_V2 = self_driving_car_pkg.sdc_V2:main',
         'lead_car_node = self_driving_car_pkg.lead_car_node:main',
         'maintain_distance_node = self_driving_car_pkg.maintain_distance_node:main',
         'front_vehicle_detector = self_driving_car_pkg.front_vehicle_detector:main',
